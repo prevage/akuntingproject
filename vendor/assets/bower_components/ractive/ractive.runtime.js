164e39923217a234d10c1f76c000c5e87a55e6ca
prevage <121592250+prevage@users.noreply.github.com>
2022-04-19 19:04:15 +0000

    powerpc: Fix 64 bit builds with binutils 2.24

---
+++ vendor/assets/bower_components/ractive/ractive.runtime.js
@@ -0,0 +1,216 @@
+/*
+	Ractive.js v0.7.3
+	Sat Apr 25 2015 13:52:38 GMT-0400 (EDT) - commit da40f81c660ba2f09c45a09a9c20fdd34ee36d80
+
+	http://ractivejs.org
+	http://twitter.com/RactiveJS
+
+	Released under the MIT License.
+*/
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  global.Ractive = factory()
+}(this, function () { 'use strict';
+
+  var TEMPLATE_VERSION = 3;
+
+  var defaultOptions = {
+
+  	// render placement:
+  	el: void 0,
+  	append: false,
+
+  	// template:
+  	template: { v: TEMPLATE_VERSION, t: [] },
+
+  	// parse:     // TODO static delimiters?
+  	preserveWhitespace: false,
+  	sanitize: false,
+  	stripComments: true,
+  	delimiters: ["{{", "}}"],
+  	tripleDelimiters: ["{{{", "}}}"],
+  	interpolate: false,
+
+  	// data & binding:
+  	data: {},
+  	computed: {},
+  	magic: false,
+  	modifyArrays: true,
+  	adapt: [],
+  	isolated: false,
+  	twoway: true,
+  	lazy: false,
+
+  	// transitions:
+  	noIntro: false,
+  	transitionsEnabled: true,
+  	complete: void 0,
+
+  	// css:
+  	css: null,
+  	noCssTransform: false
+  };
+
+  var config_defaults = defaultOptions;
+
+  // These are a subset of the easing equations found at
+  // https://raw.github.com/danro/easing-js - license info
+  // follows:
+
+  // --------------------------------------------------
+  // easing.js v0.5.4
+  // Generic set of easing functions with AMD support
+  // https://github.com/danro/easing-js
+  // This code may be freely distributed under the MIT license
+  // http://danro.mit-license.org/
+  // --------------------------------------------------
+  // All functions adapted from Thomas Fuchs & Jeremy Kahn
+  // Easing Equations (c) 2003 Robert Penner, BSD license
+  // https://raw.github.com/danro/easing-js/master/LICENSE
+  // --------------------------------------------------
+
+  // In that library, the functions named easeIn, easeOut, and
+  // easeInOut below are named easeInCubic, easeOutCubic, and
+  // (you guessed it) easeInOutCubic.
+  //
+  // You can add additional easing functions to this list, and they
+  // will be globally available.
+
+  var static_easing = {
+  	linear: function (pos) {
+  		return pos;
+  	},
+  	easeIn: function (pos) {
+  		return Math.pow(pos, 3);
+  	},
+  	easeOut: function (pos) {
+  		return Math.pow(pos - 1, 3) + 1;
+  	},
+  	easeInOut: function (pos) {
+  		if ((pos /= 0.5) < 1) {
+  			return 0.5 * Math.pow(pos, 3);
+  		}
+  		return 0.5 * (Math.pow(pos - 2, 3) + 2);
+  	}
+  };
+
+  /*global console, navigator */
+  var isClient, isJsdom, hasConsole, environment__magic, namespaces, svg, vendors;
+
+  isClient = typeof document === "object";
+
+  isJsdom = typeof navigator !== "undefined" && /jsDom/.test(navigator.appName);
+
+  hasConsole = typeof console !== "undefined" && typeof console.warn === "function" && typeof console.warn.apply === "function";
+
+  try {
+  	Object.defineProperty({}, "test", { value: 0 });
+  	environment__magic = true;
+  } catch (e) {
+  	environment__magic = false;
+  }
+
+  namespaces = {
+  	html: "http://www.w3.org/1999/xhtml",
+  	mathml: "http://www.w3.org/1998/Math/MathML",
+  	svg: "http://www.w3.org/2000/svg",
+  	xlink: "http://www.w3.org/1999/xlink",
+  	xml: "http://www.w3.org/XML/1998/namespace",
+  	xmlns: "http://www.w3.org/2000/xmlns/"
+  };
+
+  if (typeof document === "undefined") {
+  	svg = false;
+  } else {
+  	svg = document && document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1");
+  }
+
+  vendors = ["o", "ms", "moz", "webkit"];
+
+  var createElement, matches, dom__div, methodNames, unprefixed, prefixed, dom__i, j, makeFunction;
+
+  // Test for SVG support
+  if (!svg) {
+  	createElement = function (type, ns) {
+  		if (ns && ns !== namespaces.html) {
+  			throw "This browser does not support namespaces other than http://www.w3.org/1999/xhtml. The most likely cause of this error is that you're trying to render SVG in an older browser. See http://docs.ractivejs.org/latest/svg-and-older-browsers for more information";
+  		}
+
+  		return document.createElement(type);
+  	};
+  } else {
+  	createElement = function (type, ns) {
+  		if (!ns || ns === namespaces.html) {
+  			return document.createElement(type);
+  		}
+
+  		return document.createElementNS(ns, type);
+  	};
+  }
+
+  function getElement(input) {
+  	var output;
+
+  	if (!input || typeof input === "boolean") {
+  		return;
+  	}
+
+  	if (typeof window === "undefined" || !document || !input) {
+  		return null;
+  	}
+
+  	// We already have a DOM node - no work to do. (Duck typing alert!)
+  	if (input.nodeType) {
+  		return input;
+  	}
+
+  	// Get node from string
+  	if (typeof input === "string") {
+  		// try ID first
+  		output = document.getElementById(input);
+
+  		// then as selector, if possible
+  		if (!output && document.querySelector) {
+  			output = document.querySelector(input);
+  		}
+
+  		// did it work?
+  		if (output && output.nodeType) {
+  			return output;
+  		}
+  	}
+
+  	// If we've been given a collection (jQuery, Zepto etc), extract the first item
+  	if (input[0] && input[0].nodeType) {
+  		return input[0];
+  	}
+
+  	return null;
+  }
+
+  if (!isClient) {
+  	matches = null;
+  } else {
+  	dom__div = createElement("div");
+  	methodNames = ["matches", "matchesSelector"];
+
+  	makeFunction = function (methodName) {
+  		return function (node, selector) {
+  			return node[methodName](selector);
+  		};
+  	};
+
+  	dom__i = methodNames.length;
+
+  	while (dom__i-- && !matches) {
+  		unprefixed = methodNames[dom__i];
+
+  		if (dom__div[unprefixed]) {
+  			matches = makeFunction(unprefixed);
+  		} else {
+  			j = vendors.length;
+  			while (j--) {
+  				prefixed = vendors[dom__i] + unprefixed.substr(0, 1).toUpperCase() + unprefixed.substring(1);
+
